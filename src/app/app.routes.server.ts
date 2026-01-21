9e3389ea189b1b081dc31eb43a644a3ef3efe34f
Axel Charrier <axel.charrier56@ecoles-epsi.net>
2026-01-21 10:04:38 +0100

    Configuration de la route update/:id

--- src/app/app.routes.server.ts
+++ src/app/app.routes.server.ts
@@ -4,5 +4,9 @@ export const serverRoutes: ServerRoute[] = [
   {
     path: '**',
     renderMode: RenderMode.Prerender
+  },
+  {
+    path: 'update/:id',
+    renderMode: RenderMode.Server
   }
 ];
