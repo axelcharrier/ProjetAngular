6b53b829008a2af569145882d5a026a7b81aff51
Axel Charrier <axel.charrier56@ecoles-epsi.net>
2026-01-22 10:46:36 +0100

    Ajout d'un loader et gestion de la validation du formulaire d'ajout

--- src/app/components/dashboard/dashboard.ts
+++ src/app/components/dashboard/dashboard.ts
@@ -9,96 +9,109 @@ import { Router } from '@angular/router';
 import { injectForm } from '@tanstack/angular-form';
 import { TanStackField } from '@tanstack/angular-form';
 import { StudentsService } from '../../services/students-service';
+import { ProgressSpinnerModule } from 'primeng/progressspinner';
 
 @Component({
   selector: 'app-dashboard',
-  imports: [TableModule, ButtonModule, InputTextModule, ReactiveFormsModule, FloatLabelModule, TanStackField, FormsModule],
+  imports: [
+    TableModule,
+    ButtonModule,
+    InputTextModule,
+    ReactiveFormsModule,
+    FloatLabelModule,
+    TanStackField,
+    FormsModule,
+    ProgressSpinnerModule,
+  ],
   templateUrl: './dashboard.html',
 })
-
 export class Dashboard {
   students = signal<Student[]>([]);
   StudentsService: StudentsService = inject(StudentsService);
   filteredStudents = signal<Student[]>([]);
-  router = inject(Router)
+  router = inject(Router);
   testValues: boolean = false;
+  loaderStudents: boolean = true;
+  loaderNewStudents: boolean = true;
 
-  studentForm = new FormGroup({
-    id: new FormControl(),
-    lastName: new FormControl(''),
-    firstName: new FormControl(''),
-  })
-
-  
   constructor() {
     this.StudentsService.getAllData().subscribe((x) => {
       this.students.set(x);
       this.filteredStudents.set([...this.students()]);
+      this.loaderStudents = false;
     });
+  }
+
+  disableButton(): boolean {
+    if (
+      this.form.getFieldValue('firstName')?.trim() === '' ||
+      this.form.getFieldValue('lastName')?.trim() === ''
+    ) {
+      return true;
+    }
+    return false;
   }
 
   form = injectForm({
     defaultValues: {
       lastName: '',
-      firstName: ''
-    }, 
-    onSubmit: async ({value}) => {
-      if(!value.lastName || value.lastName.trim() === '' ||
-      !value.firstName || value.firstName.trim() === '')
+      firstName: '',
+    },
+    onSubmit: async ({ value }) => {
+      if (
+        !value.lastName ||
+        value.lastName.trim() === '' ||
+        !value.firstName ||
+        value.firstName.trim() === ''
+      )
         return;
 
+      this.loaderNewStudents = true;
       const studentToAdd = signal<Student>({
         id: undefined,
         lastName: value.lastName,
         firstName: value.firstName,
       });
 
-      this.StudentsService.addStudent(studentToAdd()).subscribe(x => {
-        
+      this.StudentsService.addStudent(studentToAdd()).subscribe((x) => {
         studentToAdd().id = x;
-        this.students.update((datas) => ([...datas, studentToAdd()]));
-        this.filteredStudents.update((datas) => ([...datas, studentToAdd()]));
+        this.students.update((datas) => [...datas, studentToAdd()]);
+        this.filteredStudents.update((datas) => [...datas, studentToAdd()]);
 
         this.filterResults('');
 
         this.form.reset();
-      }
-    )
-  }
-})
+
+        this.loaderNewStudents = false;
+      });
+    },
+  });
 
   handleSubmit(event: SubmitEvent) {
-    event.preventDefault()
-    event.stopPropagation()
-    this.form.handleSubmit()
+    event.preventDefault();
+    event.stopPropagation();
+    this.form.handleSubmit();
   }
 
   filterResults(text: string) {
-    if (!text)
-    {
+    if (!text) {
       this.filteredStudents.set([...this.students()]);
-      return
+      return;
     }
 
-    this.filteredStudents.set([...this.students().filter(x => x.firstName.toLowerCase().includes(text.toLowerCase()))])
+    this.filteredStudents.set([
+      ...this.students().filter((x) => x.firstName.toLowerCase().includes(text.toLowerCase())),
+    ]);
   }
 
-  removeStudent(id: number){
-    this.StudentsService.removeStudent(id).subscribe(bool => {
-      this.students.update((datas) => (datas.filter(student => student.id != id)));
-      this.filteredStudents.update((datas) => (datas.filter(student => student.id != id)))
-    })
-    
+  removeStudent(id: number) {
+    this.StudentsService.removeStudent(id).subscribe((bool) => {
+      this.students.update((datas) => datas.filter((student) => student.id != id));
+      this.filteredStudents.update((datas) => datas.filter((student) => student.id != id));
+    });
   }
 
-  buttonIsDisabled(): boolean{
-    this.studentForm.valueChanges.subscribe(test => {
-      this.testValues = (test.lastName?.trim() !== '' && test.firstName?.trim() !== '' && test.id)
-    })
-    return !this.testValues
-  }
-
-  toUpdatePage(id: number){
-    this.router.navigate(['/update', id])
+  toUpdatePage(id: number) {
+    this.router.navigate(['/update', id]);
   }
 }
